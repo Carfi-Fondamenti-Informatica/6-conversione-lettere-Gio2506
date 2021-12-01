a6c8460568ea71002a0b562523e6732653ddea23
Gio2506 <91522962+Gio2506@users.noreply.github.com>
2021-12-01 19:04:31 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
-
+#include "lib.h"
+bool ingresso (char a) {
+    if ((a >= 65 and a <= 90) or (a >= 97 and a <= 122)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+char convertitore (char a){
+    if (a>=97 and a<=122){
+        a=a-32;
+        return a;
+    }else if (a>=65 and a<=90){
+        a=a+32;
+        return a;
+    }
+}
